fe7bde67788e0b95b7dd61e3f348c1d9a7d3da59
Nilambar Sharma <nilambar@outlook.com>
2026-03-19 09:51:36 +0545

    Update dependencies

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
 
+/**
+ * wp-deployer
+ *
+ * Deploys a WordPress plugin or theme to the WordPress.org SVN repo.
+ * Config is read from package.json under the "wpDeployer" key.
+ * Run from the project root (where package.json and wpDeployer config live).
+ */
+
 import fs from 'fs-extra'
 import chalk from 'chalk'
 import merge from 'just-merge'
 import { exec } from 'child_process'
 import { waterfall } from 'async'
+import { createPluginSteps, createThemeSteps } from './lib/steps.js'
 
 const pkg = fs.readJsonSync('./package.json')
 
@@ -14,238 +23,21 @@ const noRunIfEmpty = process.platform !== 'darwin' ? '--no-run-if-empty ' : ''
 const wpDeployer = async () => {
   console.log(chalk.cyan('Processing...'))
 
-  const clearTrunk = (settings) => {
-    return function (settings, callback) {
-      console.log('Clearing trunk.')
-
-      exec(`rm -fr ${settings.svnPath}/trunk/*`, function (error, stdout, stderr) {
-        callback(null, settings)
-      })
-    }
-  }
-
-  const clearAssets = (settings) => {
-    return function (settings, callback) {
-      console.log('Clearing assets.')
-
-      exec(`rm -fr ${settings.svnPath}/assets/*`, function (error, stdout, stderr) {
-        callback(null, settings)
-      })
-    }
-  }
-
-  const prepareThemeTmp = (settings) => {
-    return function (settings, callback) {
-      console.log('Preparing temporary folder.')
-
-      exec(`rm -fr ${settings.svnPath}/`, function (error, stdout, stderr) {
-        callback(null, settings)
-      })
-    }
-  }
-
-  const clearTheme = (settings) => {
-    return function (settings, callback) {
-      console.log('Clearing theme.')
-
-      exec(`rm -fr ${settings.svnPath}/${settings.newVersion}/*`, function (error, stdout, stderr) {
-        callback(null, settings)
-      })
-    }
-  }
-
-  const checkoutDir = (dir, settings) => {
-    return function (settings, callback) {
-      console.log(`Checking out ${settings.url}${dir}/...`)
-
-      const checkoutUrl = `${settings.url}${dir}/`
-      const targetPath = `${settings.svnPath}/${dir}`
-
-      exec(`svn co --force-interactive --username="${settings.username}" ${checkoutUrl} ${targetPath}`, { maxBuffer: settings.maxBuffer }, function (error, stdout, stderr) {
-        if (error !== null) {
-          console.error(`Checkout of ${settings.url}${dir}/ unsuccessful: ${error}`)
-        } else {
-          console.log('Check out complete.')
-        }
-        callback(null, settings)
-      })
-    }
-  }
-
-  const checkoutTheme = (settings) => {
-    return function (settings, callback) {
-      console.log(`Checking out ${settings.url}...`)
-
-      const checkoutUrl = `${settings.url}/`
-      const targetPath = `${settings.svnPath}/`
-
-      exec(`svn co --force-interactive --username="${settings.username}" ${checkoutUrl} ${targetPath}`, { maxBuffer: settings.maxBuffer }, function (error, stdout, stderr) {
-        if (error !== null) {
-          console.error(`Checkout of ${settings.url}/ unsuccessful: ${error}`)
-        } else {
-          console.log('Check out complete.')
-        }
-        callback(null, settings)
-      })
-    }
-  }
-
-  const copyDirectory = (srcDir, destDir, callback) => {
-    if (srcDir.substr(-1) !== '/') {
-      srcDir = `${srcDir}/`
-    }
-
-    fs.copySync(srcDir, destDir)
-    callback()
-  }
-
-  const copyBuild = (settings) => {
-    return function (settings, callback) {
-      console.log(`Copying build directory: ${settings.buildDir}`)
-
-      copyDirectory(settings.buildDir, `${settings.svnPath}/trunk/`, function () {
-        callback(null, settings)
-      })
-    }
-  }
-
-  const copyTheme = (settings) => {
-    return function (settings, callback) {
-      console.log(`Copying build directory: ${settings.buildDir}`)
-
-      copyDirectory(settings.buildDir, `${settings.svnPath}/${settings.newVersion}/`, function () {
-        callback(null, settings)
-      })
-    }
-  }
-
-  const copyAssets = (settings) => {
-    return function (settings, callback) {
-      console.log('Copying assets')
-
-      copyDirectory(settings.assetsDir, `${settings.svnPath}/assets/`, function () {
-        callback(null, settings)
-      })
-    }
-  }
-
-  const addFiles = (settings, callback) => {
-    return function (settings, callback) {
-      console.log('Adding files in trunk')
-
-      let cmd = 'svn resolve --accept working -R . && svn status |' + awk + " '/^[?]/{print $2}' | xargs " + noRunIfEmpty + 'svn add;'
-      cmd += 'svn status | ' + awk + " '/^[!]/{print $2}' | xargs " + noRunIfEmpty + 'svn delete;'
-
-      exec(cmd, { cwd: `${settings.svnPath}/trunk` }, function (error, stdout, stderr) {
-        callback(null, settings)
-      })
-    }
-  }
-
-  const addThemeFiles = (settings, callback) => {
-    return function (settings, callback) {
-      console.log('Adding theme files')
-
-      let cmd = 'svn resolve --accept working -R . && svn status |' + awk + " '/^[?]/{print $2}' | xargs " + noRunIfEmpty + 'svn add;'
-      cmd += 'svn status | ' + awk + " '/^[!]/{print $2}' | xargs " + noRunIfEmpty + 'svn delete;'
-
-      exec(cmd, { cwd: `${settings.svnPath}/${settings.newVersion}` }, function (error, stdout, stderr) {
-        callback(null, settings)
-      })
-    }
-  }
-
-  const addAssets = (settings, callback) => {
-    return function (settings, callback) {
-      console.log('Adding assets')
-
-      let cmd = 'svn resolve --accept working -R . && svn status |' + awk + " '/^[?]/{print $2}' | xargs " + noRunIfEmpty + 'svn add;'
-      cmd += 'svn status | ' + awk + " '/^[!]/{print $2}' | xargs " + noRunIfEmpty + 'svn delete;'
-
-      exec(cmd, { cwd: `${settings.svnPath}/assets` }, function (error, stdout, stderr) {
-        callback(null, settings)
-      })
-    }
-  }
-
-  const commitToTrunk = (settings, callback) => {
-    return function (settings, callback) {
-      const commitMsg = `Committing ${settings.newVersion} to trunk`
-
-      const cmd = `svn commit --force-interactive --username="${settings.username}" -m "${commitMsg}"`
-
-      exec(cmd, { cwd: `${settings.svnPath}/trunk` }, function (error, stdout, stderr) {
-        if (error !== null) {
-          console.error(chalk.red(`Failed to commit to trunk: ${error}`))
-        }
-        callback(null, settings)
-      })
-    }
-  }
-
-  const commitToAssets = (settings, callback) => {
-    return function (settings, callback) {
-      const commitMsg = 'Committing assets'
-
-      const cmd = `svn commit --force-interactive --username="${settings.username}" -m "${commitMsg}"`
-
-      exec(cmd, { cwd: `${settings.svnPath}/assets` }, function (error, stdout, stderr) {
-        if (error !== null) {
-          console.error(chalk.red(`Failed to commit to assets: ${error}`))
-        }
-        callback(null, settings)
-      })
-    }
-  }
-
-  const commitTheme = (settings, callback) => {
-    return function (settings, callback) {
-      const commitMsg = `Committing theme ${settings.newVersion}`
-
-      console.log(commitMsg)
-
-      const cmd = `svn commit --force-interactive --username="${settings.username}" -m "${commitMsg}"`
-
-      exec(cmd, { cwd: `${settings.svnPath}/${settings.newVersion}` }, function (error, stdout, stderr) {
-        if (error !== null) {
-          console.error(chalk.red(`Failed to commit theme: ${error}`))
-        }
-        callback(null, settings)
-      })
-    }
-  }
-
-  const commitTag = (settings, callback) => {
-    return function (settings, callback) {
-      const tagCommitMsg = `Tagging ${settings.newVersion}`
-
-      console.log(tagCommitMsg)
-
-      const cmd = 'svn copy ' + settings.url + 'trunk/ ' + settings.url + 'tags/' + settings.newVersion + '/ ' + ' ' + ' --force-interactive --username="' + settings.username + '" -m "' + tagCommitMsg + '"'
-      exec(cmd, { cwd: settings.svnPath }, function (error, stdout, stderr) {
-        if (error !== null) {
-          console.error(`Failed to commit tag: ${error}`)
-        }
-        callback(null, settings)
-      })
-    }
-  }
-
-  const createThemeTag = (settings, callback) => {
-    return function (settings, callback) {
-      console.log(`Creating tag ${settings.newVersion}`)
-
-      const cmd = `svn copy ${settings.earlierVersion} ${settings.newVersion}`
-
-      exec(cmd, { cwd: settings.svnPath }, function (error, stdout, stderr) {
-        if (error !== null) {
-          console.error(`Failed to create tag: ${error}`)
-        }
-        callback(null, settings)
-      })
-    }
-  }
-
+  // Resolve config: defaults + package.json "wpDeployer"
+  // wpDeployer options (all optional except username; theme also requires earlierVersion):
+  //   slug          - Plugin/theme slug; default: package.json "name"
+  //   url           - SVN repo URL; default: plugins or themes .svn.wordpress.org from slug
+  //   username      - (required) WordPress.org SVN username
+  //   repoType      - 'plugin' | 'theme'; default: 'plugin'
+  //   buildDir      - Dir to deploy from; default: 'dist'
+  //   maxBuffer     - exec maxBuffer (bytes); default: 200*1024
+  //   deployTrunk   - (plugin) Deploy to trunk; default: true
+  //   deployTag     - (plugin) Create tag from trunk; default: true
+  //   deployAssets  - (plugin) Deploy assets dir; default: false
+  //   assetsDir     - (plugin) Dir for .wordpress-org assets; default: '.wordpress-org'
+  //   tmpDir        - Temp dir for SVN checkout; default: '/tmp/'
+  //   earlierVersion - (theme, required) Last released version for tag copy
+  //   newVersion    - Version to deploy; default: package.json "version"
   const defaults = {
     url: '',
     slug: `${pkg.name}`,
@@ -289,43 +81,14 @@ const wpDeployer = async () => {
     process.exit()
   }
 
-  let steps = []
-
-  if (settings.repoType === 'plugin') {
-    steps = [
-      function (callback) {
-        callback(null, settings)
-      },
-      settings.deployTrunk ? checkoutDir('trunk', settings) : null,
-      settings.deployTrunk ? clearTrunk(settings) : null,
-      settings.deployTrunk ? copyBuild(settings) : null,
-      settings.deployTrunk ? addFiles(settings) : null,
-      settings.deployTrunk ? commitToTrunk(settings) : null,
-
-      settings.deployTag ? commitTag(settings) : null,
-
-      settings.deployAssets ? checkoutDir('assets', settings) : null,
-      settings.deployAssets ? clearAssets(settings) : null,
-      settings.deployAssets ? copyAssets(settings) : null,
-      settings.deployAssets ? addAssets(settings) : null,
-      settings.deployAssets ? commitToAssets(settings) : null
-    ].filter(function (val) { return val !== null })
-  } else if (settings.repoType === 'theme') {
-    steps = [
-      function (callback) {
-        callback(null, settings)
-      },
-      prepareThemeTmp(settings),
-      checkoutTheme(settings),
-      createThemeTag(settings),
-      clearTheme(settings),
-      copyTheme(settings),
-      addThemeFiles(settings),
-      commitTheme(settings)
-    ].filter(function (val) { return val !== null })
-  }
+  // Build steps and run waterfall (same step sequence and callback contract as before)
+  const helpers = { exec, chalk, fs, awk, noRunIfEmpty }
+  const steps = settings.repoType === 'plugin'
+    ? createPluginSteps(settings, helpers)
+    : createThemeSteps(settings, helpers)
 
   waterfall(steps, function (err, result) {
+    if (err) console.error(chalk.red(err))
   })
 }
 
